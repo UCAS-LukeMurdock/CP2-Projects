8af599605392c441e77c24bcf303b5a538f2f9cc
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-01-08 13:43:52 -0700

    start2

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,10 +1,17 @@
 # Luke Murdock, Financial Calculator
 
 def main():
-    choice = int(input("Compoud Interest() Goal End()"))
+    choice = int(input("What do you want to use?\nBudget(1) Tip(2) Sale Price(3) Compoud Interest(4) Goal End(5)\n"))
     pass
+def budget():
 
+def tip():
 
+def sale():
+
+def compound():
+
+def goal():
 
 
 main()
