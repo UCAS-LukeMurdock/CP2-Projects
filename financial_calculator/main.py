311f21e35416240d15c788023a39f4be677fd884
Luke Murdock <luke.murdock@ucas-edu.net>
2025-01-08 22:49:35 -0700

    made budget function framework

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,17 +1,25 @@
 # Luke Murdock, Financial Calculator
 
 def main():
-    choice = int(input("What do you want to use?\nBudget(1) Tip(2) Sale Price(3) Compoud Interest(4) Goal End(5)\n"))
-    pass
+    choice = int(input("What do you want to use?\nBudget(1) Tip(2) Sale Price(3) Compoud Interest(4) Goal End(5) Done(6)\n"))
+    budget()
 def budget():
-
+    ans = float(input("How much money do you have?:"))
+    # Formats and calculates how much money should be spent in each category
+    print(f"Savings: ${(ans*0.2):,.2f}")
+    print(f"Food: ${(ans*0.1):,.2f}")
+    print(f"Bills: ${(ans*0.3):,.2f}")
+    print(f"Fun: ${(ans*0.1):,.2f}")
+    main()
 def tip():
-
+    money = 
+    tip  = 
+    pass
 def sale():
-
+    pass
 def compound():
-
+    pass
 def goal():
-
+    pass
 
 main()
