ed3f1124637c6a393452fb7ef6ee1926f9376701
Luke Murdock <luke.murdock@ucas-edu.net>
2025-01-09 19:16:12 -0700

    Finished Financial Calculator

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,9 +1,9 @@
 # Luke Murdock, Financial Calculator
 
-def main():
+def main(): # Lets user pick what they want to do with the financial calculator
     while True:
         try:
-            choice = int(input("Financial Calculator:\nBudget(1) Tip(2) Sale Price(3) Compoud Interest(4) Goal End(5) Done(6)\n"))        
+            choice = int(input("\nFinancial Calculator:\nBudget Allocator(1) Tip(2) Sale Price(3) Compoud Interest(4) Savings Goal(5) Done(6)\n"))        
         except:
             print("Invalid Input")
             continue
@@ -26,7 +26,7 @@ def main():
         print("Come back soon!")
         return
     
-def budget():
+def budget(): # Allocates a persons money into savings, bills, food, and fun
     while True:
         try:
             ans = float(input("How much money do you have?: "))
@@ -34,12 +34,12 @@ def budget():
             print("Invalid Input")
             continue
         break
+
     # Formats and calculates how much money should be spent in each category
-    print(f"Savings: ${round(ans*0.2, 2):,.2f} Bills: ${round(ans*0.5, 2):,.2f}")
-    print(f"Food: ${round(ans*0.2, 2):,.2f}")
-    print(f"Fun: ${round(ans*0.1, 2):,.2f}")
+    print(f"\nSavings: ${round(ans*0.2, 2):,.2f} | Bills: ${round(ans*0.5, 2):,.2f} | Food: ${round(ans*0.2, 2):,.2f} | Fun: ${round(ans*0.1, 2):,.2f}")
     main()
-def tip():
+
+def tip(): # Calculates how much a tip will cost and the total cost
     while True:
         try:
             cost = float(input("How much are you spending?: "))
@@ -48,9 +48,11 @@ def tip():
             print("Invalid Input")
             continue
         break
-    
-    print(f"Tip Cost: ${round(cost*tip, 2):,.2f} Total Cost: ${round(cost*tip+cost, 2):,.2f}")
-def sale():
+
+    print(f"\nTip Cost: ${round(cost*tip, 2):,.2f} | Total Cost: ${round(cost*tip+cost, 2):,.2f}")
+    main()
+
+def sale(): # Calculates how much money is saved with a discount and what is the final cost
     while True:
         try:
             cost = float(input("How much are you spending?: "))
@@ -59,22 +61,42 @@ def sale():
             print("Invalid Input")
             continue
         break
-    print(f" ${round(, 2):,.2f}")
-def comp():
-    pass
-def goal():
-    pass
 
-main()
+    print(f"\nMoney Saved: ${round(cost*disc, 2):,.2f} | Final Cost: ${round(cost-cost*disc, 2):,.2f}")
+    main()
 
-# ${round(, 2):,.2f}
-while True:
-    try:
-        a = float(input("How much are you spending?: "))
-    except:
-        print("Invalid Input")
-        continue
+def comp(): # Calculates how much money you will get
+    while True:
+        try:
+            start_money = money = float(input("How much money is in the account?: "))
+            interest = float(input("How much interest in decimal form?: "))
+            time = float(input("How many times will interest occur before you take out your money?: "))
+        except:
+            print("Invalid Input")
+            continue
+        break
+    i = 1
+    while i <= time:
+        money = money*interest+money
+        i += 1
+    print(f"\nMoney Gained: ${round(money-start_money, 2):,.2f} | Final Money: ${round(money, 2):,.2f}")
+    main()
+
+def goal(): #Tells the user how long it will take to complete their savings goal
+    while True:
+        try:
+            occur = int(input("How often are you depositing? Weekly(1) Monthly(2)\n"))
+            deposit = float(input("How much money are you depositing each time?: "))
+            goal = float(input("How much money do you want saved?: "))
+        except:
+            print("Invalid Input")
+            continue
+        if occur <= 0 or occur > 2:
+            continue
         break
 
-#
-#
+    time = round(goal/deposit)
+    print(f"\nTo reach this goal, it will take you about {time} {'week' if occur == 1 else 'month'}{'s' if time > 1 else ''}.")
+    main()
+
+main()
