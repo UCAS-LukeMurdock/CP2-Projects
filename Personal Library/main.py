5922f1f4858081ff11d6a0961e64bd2cbdb57c3d
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-01-14 13:44:12 -0700

    started functions

--- Personal Library/main.py
+++ Personal Library/main.py
@@ -1,3 +1,12 @@
 # Luke Murdock, Personal Library
 
-books = ["Fablehaven", "Dragonwatch", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",]
+books = {"Fablehaven", "Dragonwatch", "Five Kingdoms", "Beyonders", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",}
+
+def display(): #
+
+def search(): #
+
+def add(): #
+
+def remove(): #
+
