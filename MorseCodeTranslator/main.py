38725547315dea9cfa018ad93ad86c7ca61407f8
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-01-29 13:41:58 -0700

    started functins

--- MorseCodeTranslator/main.py
+++ MorseCodeTranslator/main.py
@@ -1,1 +1,32 @@
 # Luke Murdock, Simple Morse Code Translator
+
+eng_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+morse_letters = []
+
+def num_input(prompt, data_type, range = 0): # Checks and solves errors in int and float inputs
+    while True:
+        try: 
+            if data_type == "int":
+                response = int(input(prompt))
+            elif data_type == "float":
+                response = float(input(prompt))
+        except ValueError:
+            print("Invalid Input Type")
+            continue
+        if range == 0 or (response > 0 and response <= range):
+            break
+        else:
+            print("Not In Range")
+            continue
+    return response
+
+def morse():
+    h
+
+def english():
+    h
+
+def main():
+    while True:
+        h
