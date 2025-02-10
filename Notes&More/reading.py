a38cae8a3cb770889b4ddcec5f778daa25d7269b
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-02-10 13:31:36 -0700

    Reading Notes

---
+++ Notes&More/reading.py
@@ -0,0 +1,29 @@
+# Luke Murdock, Reading Notes Python
+import csv
+
+'''
+How do you open a file in your program?
+How do you alter text to work as data in a program?
+What is a CSV file Download CSV file?
+How are they used in programming?
+'''
+
+with open("Notes&More/test.txt", "r") as file:
+    content = file.read()
+    print(content[5:20].upper())
+
+print(content)
+
+file = open("Notes&More/test.txt", "r").read()
+
+users = {}
+
+with open("Notes&More/user_info.csv") as file:
+    reader = csv.reader(file)
+    print(reader)
+    next(reader)
+    for row in reader:
+        print(row)
+        users.update({row[0]:row[1]})
+print(users)
+
