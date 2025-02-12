4a076bbb049b18a8ab739c23020ce1b100dd1f16
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-02-12 13:44:07 -0700

    Added Display

--- Movie Recommender/main.py
+++ Movie Recommender/main.py
@@ -21,28 +21,52 @@ def num_input(prompt, range = 0, data_type = "int"): # Checks and solves errors 
     return response
 
 # 
-with open("Movie Recommender\Movies list.csv", "r") as file:
+with open("Movie Recommender/Movies list.csv", "r") as file:
     reader = csv.reader(file)
-    print(reader)
     for row_index, row in enumerate(reader):
         if row_index == 0:
             detail_types = row
             continue
-        print(row)
         movie = {}
         for detail_index, detail in enumerate(row):
-            #
-            movie.update({detail_types[row_index]:row[row_index]})
-        movies.append({detail_types[row_index]:row[row_index]})
-print(movies)
+            movie.update({detail_types[detail_index]:row[detail_index]})
+        movies.append(movie)
 
 def display(): # 
-    print("")
+    print("Movies:\n")
+    for movie in movies:
+        print(f"Title- {movie["Title"]}\nDirector- {movie["Director"]}\nGenre- {movie["Genre"]}\nRating- {movie["Rating"]}\nLength (min)- {movie["Length(min)"]}\nNotable Actors- {movie["Notable Actors"]}\n")
 
 def recommend(): # 
+    all_results = []
     print("\nFilter Options: Genre(1) Directors(2) Length(3) Actors(3)")
-    filter_1 = input("\nFirst Filter: ")
-    filter_2 = input("Second Filter: ")
+    filter_1 = input("\nFirst Filter: ").upper().trim()
+    filter_2 = input("Second Filter: ").upper().trim()
+    all_results.extend(searcher(filter_1))
+    all_results.extend(searcher(filter_2))
+    print("Search Results:\n")
+    if all_results == []:
+        print("None")
+    else:
+        for result in all_results:
+            print(f"Title- {result["Title"]}\nDirector- {result["Director"]}\nGenre- {result["Genre"]}\nRating- {result["Rating"]}\nLength (min)- {result["Length(min)"]}\nNotable Actors- {result["Notable Actors"]}\n")
+    
+def searcher(filter):
+    results = []
+    
+
+
+    return results
 
 def main(): # 
-    pass
+    print("Welcome, this is a program with 105 movies which can be displayed or recommended")
+    while True:
+        choice = num_input("Display(1) Recommend(2)\n")
+        if choice == 1:
+            display()
+        elif choice == 2:
+            recommend()
+        else:
+            print("Something Broke!")
+
+main()
