5fd502d7da300d6965394c376f7fecb2610a6143
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-02-20 13:31:00 -0700

    Finished

--- To Do List/main.py
+++ To Do List/main.py
@@ -1,23 +1,18 @@
 #Luke Murdock, To Do List
 import csv
 
-'''
-Create a to do list (Kept on a txt file)
-Add items to the to do list
-Mark item as complete
-Delete item from to do list
-'''
 tasks = []
-# 
-with open("To Do List/list.csv", "r") as file:
-    reader = csv.reader(file)
+# Splits the text file into lists and turns that into a list of dictionaries
+with open("To Do List/list.txt", "r") as file:
+    reader = file.read().split("\n")
     for row_index, row in enumerate(reader):
+        details = row.split("|")
         if row_index == 0:
-            detail_types = row
+            detail_types = [detail for detail in details]
             continue
         task = {}
-        for detail_index, detail in enumerate(row):
-            task.update({detail_types[detail_index]:row[detail_index]})
+        for detail in details:
+            task.update({detail_types[details.index(detail)]:detail})
         tasks.append(task)
 
 def num_input(prompt, range = 0, data_type = "int"): # Checks and solves errors in int and float inputs (Has Range If Needed)
@@ -37,21 +32,21 @@ def num_input(prompt, range = 0, data_type = "int"): # Checks and solves errors 
             continue
     return response
 
-def display(): # Prints all of the to-do list's tasks
+def display(): # Prints all of the to-do list's tasks and their status
     print("\nTo-Do List:\n")
     if tasks == []:
         print("None")
     for task in tasks:
-        print(f"Task- {task["Name"]}\nStatus- {task["Status"]}\n")
+        print(f"Task- {task['Name']}\nStatus- {task['Status']}\n")
 
-def add(): # 
+def add(): # Adds desired task as incomplete to the list
     name = input("Task: ").strip()
     tasks.append({"Name": name, "Status": "Not Done"})
+    print("Successfully Added")
     write()
     
-
-
-def mark(): # 
+def mark(): # Lets the user choose to mark or unmark the desired task as done
+    marked = False
     choice = num_input("\nMark(1) Unmark(2) Exit(3)\n", 3)
     status = "Done"
     if choice == 2:
@@ -62,17 +57,31 @@ def mark(): #
     for task in tasks:
         if name.lower() == task["Name"].lower():
             task["Status"] = status
+            marked = True
+    if marked == True:
+        print(f"Successfully {'M' if choice == 1 else 'Unm'}arked")
+    elif marked == False:
+        print(f"Unsuccessfully {'M' if choice == 1 else 'Unm'}arked")
     write()
 
-def delete(): # 
-    print("")
+def delete(): # Deletes the desired task from the list
+    deleted = False
+    name = input("Desired Task: ").strip()
+    for task in tasks:
+        if name.lower() == task["Name"].lower():
+            tasks.remove(task)
+            deleted = True
+    if deleted == True:
+        print(f"Successfully Deleted")
+    elif deleted == False:
+        print(f"Unsuccessfully Deleted")
+    write()
 
-def write(): # 
-    with open ("To Do List/list.csv", "w", newline="") as file:
-        fieldnames = ["Name", "Status"]
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(tasks)
+def write(): # Updates the file to what the user has already changed
+    with open("To Do List/list.txt", "w") as file:
+        file.write("Name|Status")
+        for task in tasks:
+            file.write(f"\n{task['Name']}|{task['Status']}")
 
 def main(): # Introduces the program and then lets the user choose one of the options
     print("Welcome to this to-do list program that lets you add, mark complete, unmark, or delete a task from the list")
@@ -85,13 +94,12 @@ def main(): # Introduces the program and then lets the user choose one of the op
         elif choice == 3:
             mark()
         elif choice == 4:
-        elif choice == 5:
             delete()
-        elif choice == 6:
+        elif choice == 5:
             print("Come Back Soon!")
             break
         else:
             print("Something Broke")
             continue
-
+        
 main()
