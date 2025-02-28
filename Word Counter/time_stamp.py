43f7ce5cc6383599547a67e308e26941b4dfc384
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-02-28 00:27:32 -0700

    Commented

--- Word Counter/time_stamp.py
+++ Word Counter/time_stamp.py
@@ -1,6 +1,6 @@
 #Luke Murdock, Time Stamp Creator
 import datetime as dt
 
-def stamp():
+def stamp(): # Uses datetime to find the exact time when the text stamp was added and then returns it in the correct format
     time = str(dt.datetime.now())
     return time[:-7]
