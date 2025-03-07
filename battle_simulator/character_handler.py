ff093f6197847a2c10e5407fd9e70457755cf402
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2025-03-07 01:01:45 -0700

    Worked on create and display and classes

--- battle_simulator/character_handler.py
+++ battle_simulator/character_handler.py
@@ -1,10 +1,36 @@
 # Luke Murdock, Character Handler
 from file_handler import read_file, write_file, intput
 
+def class_stats(user_class, charac):
+    if user_class == 1:
+        charac["Class"] = "Basic Human"
+        charac["Health"] += 1
+        charac["Strength"] += 1
+        charac["Defense"] += 1
+        charac["Speed"] += 1
+    elif user_class == 2:
+        charac["Class"] = "Knight"
+        charac["Health"] += 1
+        charac["Strength"] += 2
+        charac["Defense"] += 1
+        charac["Speed"] += -1
+    elif user_class == 3:
+        charac["Class"] = "Ranger"
+        charac["Strength"] += 2
+        charac["Defense"] += -1
+        charac["Speed"] += 2
+    elif user_class == 4:
+        charac["Class"] = "Mage"
+        charac["Health"] += 2
+        charac["Strength"] += -1
+        charac["Speed"] += 2
+    return charac
+
 def create():
     points_left = 16
     name = input("\nCharacter's Name: ")
-    print("Your character has four stats: Health, Strength, Defense, Speed\nThey are given 16 point overall and you decide which states these point are allocated to")
+    user_class = intput("\nCharacter's Class (Classes Affect Stats and Abilites):\nBasic Human(1) Knight(2) Ranger(3) Mage(4)\n", 1,4)
+    print("\nYour character has four stats: Health, Strength, Defense, Speed\nThey are given 16 point overall and you decide which stats these point are allocated to")
     def stat_input(stat_type):
         nonlocal points_left
         stat = intput(f"Character's {stat_type} Stat: ", 0,points_left)
@@ -14,18 +40,29 @@ def create():
     stren = stat_input("Strength")
     defen = stat_input("Defense")
     speed = stat_input("Speed")
-    print(f"Name: {name} H: {health} St: {stren} D: {defen} Sp: {speed}") # FIX
+    print(f"\n{name}'s Raw Stats: Health- {health} Strength- {stren} Defense- {defen} Speed- {speed}")
     retry = intput(f"You Have {points_left} points left\nRetry(1) Keep(2)\n", 1,2)
     if retry == 1:
         create()
         return
-    #ADD to character list
-    #Write that to file
-
-create()
+    charac = {
+        "Name": name,
+        "Class": "Added Soon",
+        "Level": 0,
+        "Health": health,
+        "Strength": stren,
+        "Defense": defen,
+        "Speed": speed}
+    charac = class_stats(user_class, charac)
+    print("Applied Class Stat Changes")
+    characs = read_file()
+    characs = characs.append(charac)
+    write_file(characs)
 
 def display():
-    pass
+    print("\nYour Characters")
+    for charac in read_file():
+        print(f"\n{charac["Name"]}:\nClass- {charac["Class"]}\nLevel- {charac["Level"]}\nHealth Stat- {charac["Health"]}\nStrength Stat- {charac["Strength"]}\nDefense Stat- {charac["Defense"]}\nSpeed Stat- {charac["Speed"]}")
 
 def remove():
     pass
--- battle_simulator/file_handler.py
+++ battle_simulator/file_handler.py
@@ -11,26 +11,24 @@ def read_file(): # Turns a file into a list of dictionary characters
                 continue
             dict = {}
             for detail_index, detail in enumerate(row):
-                if detail_index == 5:
-                    detail = eval(detail)
                 dict.update({detail_types[detail_index]:detail})
             characs.append(dict)
     return characs
 
-def write_file(characs): # Writes the list of dictonary  onto the file
+def write_file(characs): # Writes the list of dictonary onto the file
     with open ("battle_simulator/characters.csv", "w", newline="") as file:
-        fieldnames = ["Name","Streak","Day","Coins","Gems","Bought","Storage"]
+        fieldnames = ["Name","Class","Level","Health","Strength","Defense","Speed"]
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(characs)
 
-def intput(prompt, min = 0, max = 0): # Checks and prompts user to solve errors in integer inputs (Has Range If Needed)
+def intput(prompt, min = -1, max = -1): # Checks and prompts user to solve errors in integer inputs (Has Range If Needed)
     try:
         response = int(input(prompt).strip())
     except:
         print("Not An Integer")
         response = intput(prompt,min,max)
-    if (min != 0 or max != 0) and (response < min or response > max): # If either min or max aren't 0 and the input is out of range then the user has to reinput
+    if (min != -1 or max != -1) and (response < min or response > max): # If either min or max aren't -1 and the input is out of range then the user has to reinput
         print(f"Not In Range: {min}-{max}")
         response = intput(prompt,min,max)
     return response
